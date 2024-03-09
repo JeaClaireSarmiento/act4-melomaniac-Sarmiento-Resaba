ae6118e02b70fb19cdbd90cf303170c72bad84a5
SarmientoJeaClaire <sarmientojeaclaire@gmail.com>
2024-03-09 20:28:25 +0800

    song5.php

---
+++ song5.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>song5</title>
+    <style>
+        body {
+            font-family: "Times New Roman", Times, serif;
+            font-size: 14px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            text-align: left;
+            background-color: lavender;
+        }
+        .song-details {
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            margin-top: 10px;
+        }
+        .song-image {
+            width: 200px;
+            height: 200px;
+        }
+    </style>
+</head>
+<body>
+    <img class="song-image" src="song5.jpg" alt="Thumbnail for Questions for the Universe by Laufey">
+    <div class="song-details">
+        <h1>Questions for the Universe</h1>
+        <h2>by Laufey</h2>
+    </div>
+    <?php
+    echo "<pre>";
+    echo "[Intro]</p>";
+    echo "<p>G  Am  D6  D  G</p>";
+    echo "<br>";
+    echo "<p>[Verse 1]</p>";
+    echo "<p>Gmaj7  Am7  Bm7      E7      Am7    D7  D7b9</p>";
+    echo "<p>Why do I love the moon so much?</p>";
+    echo "<p>Gmaj7 Am7  Bm7      E7        Am7    D7  D7b9</p>";
+    echo "<p>How come I crave a stranger's touch?</p>";
+    echo "<p>Bm7b5                E7</p>";
+    echo "<p>lately, love's been like a curse</p>";
+    echo "<p>  Am7                Cm</p>";
+    echo "<p>I speak my mind and don't think first</p>";
+    echo "<p>Bm7b5         E7       Am7     D7     Gmaj7</p>";
+    echo "<p>These are my questions for the universe</p>";
+    echo "<p>Am7  D7b9</p>";
+    echo "<br>";
+    echo "<p>[Verse 2]</p>";
+    echo "<p>Gmaj7  Am7  Bm7        E7      Am7    D7  D7b9</p>";
+    echo "<p>Why do I sleep all day and night?</p>";
+    echo "<p>Gmaj7  Am7           Bm7      E7      Am7    D7  D7b9</p>";
+    echo "<p>Live in this puzzling age and time</p>";
+    echo "<p>Bm7b5                   E7</p>";
+    echo "<p>Why would I kiss a boy just to get hurt?</p>";
+    echo "<p>Am7                        Cm</p>";
+    echo "<p>Plant a living flower on this dying earth?</p>";
+    echo "<p>Bm7b5         E7       Am7     D7   G/D  Am/E  Bbm/F  G</p>";
+    echo "<p>These are my questions for the universe</p>";
+    echo "<br>";
+    echo "<p>[Bridge]</p>";
+    echo "<p>Cm7          F7                 Bbdim   Bb</p>";
+    echo "<p>Falling in love feels more like crashing</p>";
+    echo "<p>Bbm7       Eb7             Abdim  Ab  Gsus4  Gaug</p>";
+    echo "<p>Stars spin around my head, so     I'm ask-----ing</p>";
+    echo "<br>";
+    echo "<p>[Verse 3]</p>";
+    echo "<p>Gmaj7   Am7     Bm7  E7     Am7   D7  D7b9</p>";
+    echo "<p>Will my worries ever melt away?</p>";
+    echo "<p>Gmaj7 Am7   Bm7   E7    Am7  D7  D7b9</p>";
+    echo "<p>Run out of sentences to say</p>";
+    echo "<p>Bm7b5                        E7</p>";
+    echo "<p>Wish that I could put this into better words</p>";
+    echo "<p>       Am7                          Cm</p>";
+    echo "<p>Will I ever fall in love or will I always search?</p>";
+    echo "<p>Bm7b5         E7       Am7     D7     Bm7   E7</p>";
+    echo "<p>These are my questions for the universe</p>";
+    echo "<p>Am7                    D7          Gmaj7</p>";
+    echo "<p>These are my questions for the universe</p>";
+    
+    ?>
+    <a href="index.php">Go Back</a>
+</body>
+</html>
