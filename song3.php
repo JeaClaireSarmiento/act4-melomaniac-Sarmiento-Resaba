de4cfe3e2be736df9685d67a82837bedddbde1c6
SarmientoJeaClaire <sarmientojeaclaire@gmail.com>
2024-03-09 20:27:28 +0800

    song3.php

---
+++ song3.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>song3</title>
+    <style>
+        body {
+            font-family: "Times New Roman", Times, serif;
+            font-size: 14px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            text-align: left;
+            background-color: lavender;
+        }
+        .song-details {
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            margin-top: 10px;
+        }
+        .song-image {
+            width: 200px;
+            height: 200px;
+        }
+    </style>
+</head>
+<body>
+    <img class="song-image" src="song3.jpg" alt="Thumbnail for Ballad of a Homeschooled Girl by Olivia Rodrigo">
+    <div class="song-details">
+        <h1>Ballad of a Homeschooled Girl</h1>
+        <h2>by Olivia Rodrigo</h2>
+    </div>
+    <?php
+    echo "<pre>";
+    echo "<p>[Intro]</p>";
+    echo "<p>G F G F</p>";
+    echo "<p>G F G F</p>";
+    echo "<br>";
+    echo "<p>[Verse 1]</p>";
+    echo "<p>G  F       G      F</p>";
+    echo "<p>Cat got my tongue</p>";
+    echo "<p>            G            F            G   F</p>";
+    echo "<p>And I don't think I get along with anyone</p>";
+    echo "<p>G    F        G    F</p>";
+    echo "<p>Blood running cold</p>";
+    echo "<p>           G              F          G         F</p>";
+    echo "<p>I'm on the outside of the greatest inside joke</p>";
+    echo "<p>      G   F       G       F</p>";
+    echo "<p>And I hate all my clothes</p>";
+    echo "<p>              G                F            G      F</p>";    
+    echo "<p>Feels like my skin doesn't fit right over my bones</p>";
+    echo "<p>     G    F         G   F</p>";
+    echo "<p>So I guess I should go</p>";
+    echo "<p>            G                F              G</p>";
+    echo "<p>The party's done, and I'm no fun, I know, I know</p>";
+    echo "<p>  F</p>";
+    echo "<p>I know, I know</p>";
+    echo "<br>";
+    echo "<p>[Chorus]</p>";
+    echo "<p>G          Em                   Bm</p>";
+    echo "<p> I broke a glass, I tripped and fell</p>";
+    echo "<p>         C                G</p>";
+    echo "<p>I told secrets I shouldn't tell</p>";
+    echo "<p>           Em          Bm</p>";
+    echo "<p>I stumbled over all my words</p>";
+    echo "<p>          C                G</p>";
+    echo "<p>I made it weird, I made it worse</p>";
+    echo "<p>     Em     Bm      C</p>";
+    echo "<p>Each time I step outside</p>";
+    echo "<p>G     Em     Bm N.C.  G Em Bm</p>";
+    echo "<p> It's social suicide</p>";
+    echo "<p>            C</p>";
+    echo "<p>It's social suicide</p>";
+    echo "<p>G           Em         Bm</p>";
+    echo "<p>Wanna curl up and die</p>";
+    echo "<p>            C</p>";
+    echo "<p>It's social suicide</p>";
+    echo "<br>";
+    echo "<p>[Post-Chorus]</p>";
+    echo "<p>   G F    G  F</p>";
+    echo "<p>Ah-ah, ah-ah</p>";
+    echo "<p>   G        F         G  F</p>";
+    echo "<p>Ah-ah-ah-ah-ah-ah, ah-ah</p>";
+    echo "<br>";
+    echo "<p>[Verse 2]</p>";
+    echo "<p>  G                          F</p>";
+    echo "<p>I laughed at the wrong time, sat with the wrong guy (Uh-huh)</p>";
+    echo "<p>G                               F</p>";
+    echo "<p>Searchin' 'how to start a conversation?' on a website (How to flirt?)";
+    echo "<p>  G                       F</p>";
+    echo "<p>I talked to this hot guy, swore I was his type</p>";
+    echo "<p>G                                 F</p>";
+    echo "<p>Guess that he was makin' out with boys, like the whole night (Oh)</p>";
+    echo "<p>G             F</p>";
+    echo "<p> Everythin' I do is tragic (Oh)</p>";
+    echo "<p>G            F</p>";
+    echo "<p> Every guy I like is gay (Oh)</p>";
+    echo "<p>G            F</p>";
+    echo "<p> The morning after I panic (Oh)</p>";
+    echo "<p>G            F</p>";
+    echo "<p> Oh God, what did I say? (Oh, oh, oh)</p>";
+    echo "<br>";
+    echo "<p>[Chorus]</p>";
+    echo "<p>G          Em                   Bm</p>";
+    echo "<p> I broke a glass, I tripped and fell</p>";
+    echo "<p>          C                G</p>";
+    echo "<p>I told secrets I shouldn't tell</p>";
+    echo "<p>           Em          Bm</p>";
+    echo "<p>I stumbled over all my words</p>";
+    echo "<p>          C                G</p>";
+    echo "<p>I made it weird, I made it worse</p>";
+    echo "<p>     Em     Bm      C</p>";
+    echo "<p>Each time I step outside</p>";
+    echo "<p>G     Em     Bm N.C.  G Em Bm</p>";
+    echo "<p> It's social suicide</p>";
+    echo "<p>            C</p>";
+    echo "<p>It's social suicide</p>";
+    echo "<p>G           Em         Bm</p>";
+    echo "<p> Wanna curl up and die</p>";
+    echo "<p>            C</p>";
+    echo "<p>It's social suicide</p>";
+    echo "<p>G               Em</p>";
+    echo "<p>Yeah, when I'm alone, I'm fine</p>";
+    echo "<p>    Bm           C</p>";
+    echo "<p>But don't let me out at night</p>";
+    echo "<p>G            Em</p>";
+    echo "<p> It's social suicide</p>";
+    echo "<p>Bm            C</p>";
+    echo "<p> It's social suicide</p>";
+    echo "<br>";
+    echo "<p>[Post-Chorus]</p>";
+    echo "<p>   G F    G  F</p>";
+    echo "<p>Ah-ah, ah-ah</p>";
+    echo "<p>   G        F         G  F</p>";
+    echo "<p>Ah-ah-ah-ah-ah-ah, ah-ah</p>";
+    echo "<p>   G F    G  F</p>";
+    echo "<p>Ah-ah, ah-ah</p>";
+    echo "<p>   G        F         G  F</p>";
+    echo "<p>Ah-ah-ah-ah-ah-ah, ah-ah</p>";
+    echo "<br>";
+    echo "<p>[Chorus]</p>";
+    echo "<p>G          Em                   Bm</p>";
+    echo "<p> I broke a glass, tripped and fell</p>";
+    echo "<p>       C                 G</p>";
+    echo "<p>Told secrets I shouldn't tell</p>";
+    echo "<p>         Em          Bm</p>";
+    echo "<p>Stumbled over all my words</p>";
+    echo "<p>        C                   G</p>";
+    echo "<p>Made it weird, then made it worse</p>";
+    echo "<p>     Em       Bm   C</p>";
+    echo "<p>Each day that I'm alive</p>";
+    echo "<p>G     Em     Bm N.C.  G Em Bm</p>";
+    echo "<p> It's social suicide</p>";
+    echo "<p>            C</p>";
+    echo "<p>It's social suicide</p>";
+    echo "<p>G           Em         Bm</p>";
+    echo "<p> Wanna curl up and die</p>";
+    echo "<p>            C</p>";
+    echo "<p>It's social suicide</p>";
+    echo "<p>G            Em</p>";
+    echo "<p> It's social suicide</p>";
+    echo "<p>Bm           C          G</p>";
+    echo "<p>Don't let me out at night</p>";
+    echo "<p>                Em</p>";
+    echo "<p>I'm shocked I'm still alive</p>";
+    echo "<p>Bm             C</p>";
+    echo "<p>  It's social suicide</p>";
+    echo "<br>";
+    echo "<p>[Post-Chorus]</p>";
+    echo "<p>   G F    G  F</p>";
+    echo "<p>Ah-ah, ah-ah</p>";
+    echo "<p>   G        F         G  F</p>";
+    echo "<p>Ah-ah-ah-ah-ah-ah, ah-ah</p>";
+    echo "<br>";
+    echo "<p>[Outro]</p>";
+    echo "<p>G                 F</p>";
+    echo "<p> Thought your mom was your wife (Ah)</p>";
+    echo "<p>G               F</p>";
+    echo "<p> Called you the wrong name twice (Ah)</p>";
+    echo "<p>G                 F</p>";
+    echo "<p> Can't think of a third line (Ah)</p>";
+    echo "<p>G         F</p>";
+    echo "<p> La-la-la-la-la-la (Ah)</p>";
+    echo "<p>G         F</p>";
+    echo "<p> La-la-la-la-la-la (Ah)</p>";
+    echo "<p>G         F</p>";
+    echo "<p> La-la-la-la-la-la (Ah)</p>";
+    echo "<p>G</p>";
+    echo "<p> La-la-la-la-la-la, ugh</p>";
+    
+    ?>
+    <a href="index.php">Go Back</a>
+</body>
+</html>
