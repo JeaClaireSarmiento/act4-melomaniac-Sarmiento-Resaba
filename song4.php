e8b042a069b56431233829a8c5a9b6d7bc227ff7
SarmientoJeaClaire <sarmientojeaclaire@gmail.com>
2024-03-09 20:28:01 +0800

    song4.php

---
+++ song4.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>song4</title>
+    <style>
+        body {
+            font-family: "Times New Roman", Times, serif;
+            font-size: 14px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            text-align: left;
+            background-color: lavender;
+        }
+        .song-details {
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            margin-top: 10px;
+        }
+        .song-image {
+            width: 200px;
+            height: 200px;
+        }
+    </style>
+</head>
+<body>
+    <img class="song-image" src="song4.jpg" alt="Thumbnail for Bewitched by Laufey">
+    <div class="song-details">
+        <h1>Bewitched</h1>
+        <h2>by Laufey</h2>
+    </div>
+    <?php
+    echo "<pre>";
+    echo "<p>[Intro]</p>";
+    echo "<p>Db</p>";
+    echo "<br>";
+    echo "<p>[Verse 1]</p>";
+    echo "<p>                   Bbm7</p>";
+    echo "<p>Wrapped me in your arms</p>";
+    echo "<p>              Ebm9 Ab13</p>";
+    echo "<p>Leaned in and whispered</p>";
+    echo "<p> Db              Bbm7            Ebm9 Ab13</p>";
+    echo "<p>Keep me in your heart, I'm so bewil--dered</p>";
+    echo "<p>Fm7               Bb7(b13)</p>";
+    echo "<p>What's this new desire called?</p>";
+    echo "<p>  Ebm9             Gbm7</p>";
+    echo "<p>I didn't know that much at all</p>";
+    echo "<p>      Fm7               Bbm7</p>";
+    echo "<p>'Bout love before, But now I think</p>";
+    echo "<p>    Ebm9 Ab13</p>";
+    echo "<p>I'm learning</p>";
+    echo "<br>";
+    echo "<p>[Chorus]</p>";
+    echo "<p>      Db</p>";
+    echo "<p>You bewitched me</p>";
+    echo "<p>                             Bbm7</p>";
+    echo "<p>From the first time that you kissed me</p>";
+    echo "<p>                          Ebm9</p>";
+    echo "<p>Waited all night,then we ran down the street in the late London light</p>";
+    echo "<p>    Ab13</p>";
+    echo "<p>The world froze around us, you kissed me goodnight</p>";
+    echo "<p>      Db</p>";
+    echo "<p>You bewitch me</p>";
+    echo "<p>                         Bbm7</p>";
+    echo "<p>Every damn second you're with me</p>";
+    echo "<p>I try to think straight</p>";
+    echo "<p>        Ebm9</p>";
+    echo "<p>But I'm falling so badly, I'm coming apart</p>";
+    echo "<p>    Ab13</p>";
+    echo "<p>You wrote me a note, cast a spell on my heart</p>";
+    echo "<p>     Bbm7     Abm7 Eb7</p>";
+    echo "<p>And bewitched me</p>";
+    echo "<p>Ebm7 Ab13 Ab7 Db   Bbm7</p>";
+    echo "<p>Bewitched me</p>";
+    echo "<br>";
+    echo "<p>[Verse 2]</p>";
+    echo "<p>Db                Bbm7</p>";
+    echo "<p>You're not even gone</p>";
+    echo "<p>          Ebm9 Ab13</p>";
+    echo "<p>I already miss you</p>";
+    echo "<p>Db           Bbm7</p>";
+    echo "<p>What's going on?</p>";
+    echo "<p>           Ebm9 Ab13</p>";
+    echo "<p>I've never been through</p>";
+    echo "<p>Fm7                 Bb7(b13)</p>";
+    echo "<p>This all-consuming fire fuming</p>";
+    echo "<p>Ebm9       Gbm7</p>";
+    echo "<p>Cursing at the moon and</p>";
+    echo "<p>        Fm7             Bbm7</p>";
+    echo "<p>Loosing all control and crying</p>";
+    echo "<p>        Ebm9      Ab13</p>";
+    echo "<p>'Cause I think I'm falling</p>";
+    echo "<br>";
+    echo "<p>[Chorus]</p>";
+    echo "<p>      Db</p>";
+    echo "<p>You bewitched me</p>";
+    echo "<p>                             Bbm7</p>";
+    echo "<p>From the first time that you kissed me</p>";
+    echo "<p>                          Ebm9</p>";
+    echo "<p>Waited all night, then we ran down the street in the late London light</p>";
+    echo "<p>    Ab13</p>";
+    echo "<p>The world froze around us, you kissed me goodnight</p>";
+    echo "<p>      Db</p>";
+    echo "<p>You bewitch me</p>";
+    echo "<p>                         Bbm7</p>";
+    echo "<p>Every damn second you're with me</p>";
+    echo "<p>I try to think straight</p>";
+    echo "<p>        Ebm9</p>";
+    echo "<p>But I'm falling so badly, I'm coming apart</p>";
+    echo "<p>    Ab13</p>";
+    echo "<p>You wrote me a note, cast a spell on my heart</p>";
+    echo "<p>     Bbm7    Ab7 Eb7</p>";
+    echo "<p>And bewitched me</p>";
+    echo "<br>";
+    echo "<p>[Outro]</p>";
+    echo "<p> Ebm7</p>";
+    echo "<p>Bewitched</p>";
+    echo "<p>     Ab13   Db</p>";
+    echo "<p>You bewitched me</p>";
+    
+    ?>
+    <a href="index.php">Go Back</a>
+</body>
+</html>
