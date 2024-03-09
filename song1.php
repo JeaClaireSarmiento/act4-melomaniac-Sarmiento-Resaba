161757576db1d313b40861528283bcd96355f735
SarmientoJeaClaire <sarmientojeaclaire@gmail.com>
2024-03-09 20:26:38 +0800

    song1.php

---
+++ song1.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>song1</title>
+    <style>
+        body {
+            font-family: "Times New Roman", Times, serif;
+            font-size: 14px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            text-align: left;
+            background-color: lavender;
+        }
+        .song-details {
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            margin-top: 10px;
+        }
+        .song-image {
+            width: 200px;
+            height: 200px;
+        }
+    </style>
+</head>
+<body>
+    <img class="song-image" src="song1.png" alt="Thumbnail for Tumitigil ang Mundo by BGYO">
+    <div class="song-details">
+        <h1>Tumitigil ang Mundo</h1>
+        <h2>by BGYO</h2>
+    </div>
+    <?php
+    echo "<pre>";
+    echo "<p>[Verse1]</p>"; 
+    echo "<p>C        Am          F       G<p>";
+    echo "<p>Kanina pa ako sa'yo sumusulyap<p>";
+    echo "<p>C        Am          F       G</p>";
+    echo "<p>Kahit titigan kita magdamag</p>";
+    echo "<p>C        Am          F       G</p>";
+    echo "<p>Langit lagi pakiramdam kapag</p>";
+    echo "<p>C  Am        F         G</p>";
+    echo "<p>Ika'y kasama</p>";
+    echo "<p>         C        Am              F       G</p>";
+    echo "<p>Pilitin man at pigilan ka't ang nararamdaman</p>";
+    echo "<p>        C        Am          F       G</p>";
+    echo "<p>Mas madali na aminin 'yon din ang pupuntahan</p>";
+    echo "<p>       C Am      F      G</p>";
+    echo "<p>Oh 'di ba o 'di nga ba?</p>";
+    echo "<p>          C      Am        F   G</p>";
+    echo "<p>Kasi sa t'wing makakasama ka</p>";
+    echo "<br>";
+    echo "<p>[Pre-Chorus]</p>";
+    echo "<p>          C      G</p>";
+    echo "<p>Madaming nakakalimutan</p>";
+    echo "<p>      Am           F</p>";
+    echo "<p>Nawawala lahat sa isipanp>";
+    echo "<p>C</p>";
+    echo "<p>'Di ko alam ang gagawin</p>";
+    echo "<p>G</p>";
+    echo "<p>Kapag ika'y nakatingin</p>";
+    echo "<p>       Am                    F</p>";
+    echo "<p>Kasi alam mo ba nang dahil sa'yo</p>";
+    echo "<br>";
+    echo "<p>[Chorus]</p>";
+    echo "<p>                C</p>";
+    echo "<p>Tumitigil ang mundo, ohh</p>";
+    echo "<p>G                 Am   F</p>";
+    echo "<p>Sa kislap ng mata mo</p>";
+    echo "<p>                     C</p>";
+    echo "<p>Makarating man kung saan</p>";
+    echo "<p>                 G</p>";
+    echo "<p>Ikaw lang babalikan</p>";
+    echo "<p>           Am    F</p>";
+    echo "<p>Asahan mo ito</p>";
+    echo "<p>                C</p>";
+    echo "<p>Tumitigil ang mundo, ohh</p>";
+    echo "<p>G                 Am   F</p>";
+    echo "<p>Sa kislap ng mata mo</p>";
+    echo "<p>                     C</p>";
+    echo "<p>Makarating man kung saan</p>";
+    echo "<p>                 G</p>";
+    echo "<p>Ikaw lang babalikan</p>";
+    echo "<p>           Am       F>/p>";
+    echo "<p>Asahan mo ito , oh-ohh, oh-oh</p>";
+    echo "<br>";
+    echo "<p>[Verse 2]</p>";
+    echo "<p>C        Am          F       G</p>";
+    echo "<p>Bawat ngiti ako'y hindi mapakali</p>";
+    echo "<p>C        Am          F       G</p>";
+    echo "<p>Hanap palagi ikaw bawat sandali</p>";
+    echo "<p>C        Am          F       G</p>";
+    echo "<p>Apoy sa titig ko ikaw ang nagsindi</p>";
+    echo "<p>C        Am          F       G</p>";
+    echo "<p>Ikaw lang nais makatabi</p>";
+    echo "<p>         C        Am              F       G</p>";
+    echo "<p>Pilitin man at pigilan ka't ang nararamdaman</p>";
+    echo "<p>        C        Am          F       G</p>";
+    echo "<p>Mas madali na aminin 'yon din ang pupuntahan</p>";
+    echo "<p>       C Am      F      G</p>";
+    echo "<p>Oh 'di ba o 'di nga ba?</p>";
+    echo "<p>          C      Am        F   G</p>";
+    echo "<p>Kasi sa t'wing makakasama ka</p>";
+    echo "<br>";
+    echo "<p>[Pre-Chorus]</p>";
+    echo "<p>          C      G</p>";
+    echo "<p>Madaming nakakalimutan</p>";
+    echo "<p>Sa kislap ng mata mo</p>";
+    echo "<p>                     C</p>";
+    echo "<p>Makarating man kung saan</p>";
+    echo "<p>      Am           F</p>";
+    echo "<p>Nawawala lahat sa isipan</p>";
+    echo "<p>C>/p>";
+    echo "<p>'Di ko alam ang gagawin</p>";
+    echo "<p>G</p>";
+    echo "<p>Kapag ika'y nakatingin</p>";
+    echo "<p>       Am                    F</p>";
+    echo "<p>Kasi alam mo ba nang dahil sa'yo</p>";
+    echo "<br>";
+    echo "<p>[Chorus]</p>";
+    echo "<p>                C</p>";
+    echo "<p>Tumitigil ang mundo, ohh</p>";
+    echo "<p>G                 Am   F</p>";
+    echo "<p>Sa kislap ng mata mo</p>";
+    echo "<p>                     C</p>";
+    echo "<p>Makarating man kung saan</p>";
+    echo "<p>                 G</p>";
+    echo "<p>Ikaw lang babalikan</p>";
+    echo "<p>           Am    F</p>";
+    echo "<p>Asahan mo ito</p>";
+    echo "<p>                C</p>";
+    echo "<p>Tumitigil ang mundo, ohh</p>";
+    echo "<p>G                 Am   F</p>";
+    echo "<p>Sa kislap ng mata mo</p>";
+    echo "<p>                     C</p>";
+    echo "<p>Makarating man kung saan</p>";
+    echo "<p>                 G</p>";
+    echo "<p>Ikaw lang babalikan</p>";
+    echo "<p>           Am       F</p>";
+    echo "<p>Asahan mo ito , oh-ohh, oh-oh</p>";
+    echo "<br>";
+    echo "<p>[Bridge]</p>";
+    echo "<p>C        Am            F       G</p>";
+    echo "<p>Daming nakilala kahit isa 'di umubra</p>";
+    echo "<p>         C           Am</p>";
+    echo "<p>Naglibot na wala talaga</p>";
+    echo "<p>         F             G</p>";
+    echo "<p>Mas maganda ka sa'king mata</p>";
+    echo "<p>         C         Am</p>";
+    echo "<p>Sa'yo lang nakatingin</p>";
+    echo "<p>         F               G</p>";
+    echo "<p>Nagpapapansin sa isang bituin</p>";
+    echo "<p>         C               Am</p>";
+    echo "<p>'Di ko pa rin kayang alisin</p>";
+    echo "<p>         F               G</p>";
+    echo "<p>Aking tingin sa iyong ningning</p>";
+    echo "<p>        C     Am          F     G</p>";
+    echo "<p>'Di ko maikaila ikaw lang talaga</p>";
+    echo "<p>      C     Am          F     G</p>";
+    echo "<p>Sabihin man sa akin ay 'di bale na</p>";
+    echo "<p>        C     Am          F     G</p>";
+    echo "<p>Pigilan man ng marami, andito lang palagi</p>";
+    echo "<p>        C     Am          F     G</p>";
+    echo "<p>Sa'king mundo ikaw nagmamay-ari</p>";
+    echo "<br>";
+    echo "<p>[Pre-Chorus]</p>";
+    echo "<p>          C      G</p>";
+    echo "<p>Madaming nakakalimutan</p>";
+    echo "<p>      Am           F</p>";
+    echo "<p>Nawawala lahat sa isipan</p>";
+    echo "<p>C</p>";
+    echo "<p>'Di ko alam ang gagawin</p>";
+    echo "<p>G</p>";
+    echo "<p>Kapag ika'y nakatingin</p>";
+    echo "<p>       Am                    F</p>";
+    echo "<p>Kasi alam mo ba nang dahil sa'yo</p>";
+    echo "<br>";
+    echo "<p>[Chorus]</p>";
+    echo "<p>                C</p>";
+    echo "<p>Tumitigil ang mundo, ohh</p>";
+    echo "<p>G                 Am   F</p>";
+    echo "<p>Sa kislap ng mata mo (Sa kislap ng mata mo)</p>";
+    echo "<p>                     C</p>";
+    echo "<p>Makarating man kung saan (Saan)</p>";
+    echo "<p>                 G</p>";
+    echo "<p>Ikaw lang babalikan (Balikan)</p>";
+    echo "<p>           Am    F</p>";
+    echo "<p>Asahan mo ito (Asahan mo ito)</p>";
+    echo "<p>                C</p>";
+    echo "<p>Tumitigil ang mundo, ohh (Tumitigil ang mundo oh oh)</p>";
+    echo "<p>G                 Am   F</p>";
+    echo "<p>Sa kislap ng mata mo (mo oh)</p>";
+    echo "<p>                     C</p>";
+    echo "<p>Makarating man kung saan (saan)</p>";
+    echo "<p>                 G</p>";
+    echo "<p>Ikaw lang babalikan</p>";
+    echo "<p>           Am       F</p>";
+    echo "<p>Asahan mo ito (Asahan mo ito)</p>";
+    
+    ?>
+    <a href="index.php">Go Back</a>
+</body>
+</html>
