352bd40221ad9ea34d1062f4f7b7d56fd4f278b4
SarmientoJeaClaire <sarmientojeaclaire@gmail.com>
2024-03-09 20:27:03 +0800

    song2.php

---
+++ song2.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>song2</title>
+    <style>
+        body {
+            font-family: "Times New Roman", Times, serif;
+            font-size: 14px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            text-align: left;
+            background-color: lavender;
+        }
+        .song-details {
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            margin-top: 10px;
+        }
+        .song-image {
+            width: 200px;
+            height: 200px;
+        }
+    </style>
+</head>
+<body>
+    <img class="song-image" src="song2.jpg" alt="Thumbnail for Kumpas by Moira Dela Torre">
+    <div class="song-details">
+        <h1>Kumpas</h1>
+        <h2>by Moira Dela Torre</h2>
+    </div>
+    <?php
+    echo "<pre>";
+    echo "<p>[Intro]</p>"; 
+    echo "<p>F Bb C x2</p>";
+    echo "<br>";
+    echo "<p>[Verse]</p>";
+    echo "<p>     F</p>";
+    echo "<p>Pano bang mababawi</p>";
+    echo "<p>  Bb     C       F  Bb C</p>";
+    echo "<p>Lahat ng mga nasabi</p>";
+    echo "<p>     F</p>";
+    echo "<p>Di naman inakalang</p>";
+    echo "<p> Bb       C           F</p>";
+    echo "<p>Ika’y darating lang bigla</p>";
+    echo "<p>              Bb C</p>";
+    echo "<p>Ng walang babala</p>";
+    echo "<p>[Pre-Chorus]</p>";
+    echo "<p>    Dm     A</p>";
+    echo "<p>Sa isang iglap</p>";
+    echo "<p>    C         G7</p>";
+    echo "<p>Nag bago’ng lahat</p>";
+    echo "<p>   Gm7      Am7</p>";
+    echo "<p>Hindi ko na kaya</p>";
+    echo "<p>   Bb        C</p>";
+    echo "<p>Pa na magpanggap</p>";
+    echo "<br>";
+    echo "<p>[Chorus]</p>";
+    echo "<p>            F             C</p>";
+    echo "<p>Ikaw ang kumpas pag naliligaw</p>"; 
+    echo "<p>           Bb                  C</p>";
+    echo "<p>Ikaw ang kulay sa langit na bughaw</p>"; 
+    echo "<p>           F            C</p>";
+    echo "<p>Sa bawat bagyo na dumadayo</p>";
+    echo "<p>            Bb                  C</p>";
+    echo "<p>Ikaw ang kanlungan na kailangan ko</p>";
+    echo "<p>         Dm     C</p>";
+    echo "<p>Kahit hindi mo alam</p>";
+    echo "<p>      F        Gm7      Bb,/p>";
+    echo "<p>Ilang beses mo kong niligtas</p>";
+    echo "<p> Gm7            C              F</p>";
+    echo "<p>Ikaw ang hantungan, at aking wakas</p>";
+    echo "<br>";
+    echo "<p>[Instrumental]</p>";
+    echo "<p>Bb C</p>";
+    echo "<p>F Bb C</p>";
+    echo "<br>";
+    echo "<p>[Verse]</p>";
+    echo "<p>       F</p>";
+    echo "<p>Panong maniniwala</p>";
+    echo "<p> Bb       C        F   Bb C</p>";
+    echo "<p>Ika’y nasaking harapan</p>";
+    echo "<p>     F</p>";
+    echo "<p>Di naman naiplano</p>";
+    echo "<p> Bb       C         F</p>";
+    echo "<p>Ako’y mabihag ng ganto</p>";
+    echo "<p>         Bb  C</p>";
+    echo "<p>Totoo ba ito?</p>";
+    echo "<br>";
+    echo "<p>[Pre-Chorus]<p>";
+    echo "<p>    Dm     A</p>";
+    echo "<p>Sa isang iglap</p>";
+    echo "<p>    C    G7</p>";
+    echo "<p>Nag bago ako</p>";
+    echo "<p>   Gm7      Am7</p>";
+    echo "<p>Hindi ko na kayang</p>";
+    echo "<p>  Bb       C</p>";
+    echo "<p>Mawalay pa sayo</p>";
+    echo "<br>";
+    echo "<p>[Chorus]</p>";
+    echo "<p>            F             C</p>";
+    echo "<p>Ikaw ang kumpas pag naliligaw</p>";
+    echo "<p>           Bb                  C</p>";
+    echo "<p>Ikaw ang kulay sa langit na bughaw</p>";
+    echo "<p>           F            C</p>";
+    echo "<p>Sa bawat bagyo na dumadayo</p>";
+    echo "<p>            Bb                  C</p>";
+    echo "<p>Ikaw ang kanlungan na kailangan ko</p>";
+    echo "<p>         Dm     C<p>";
+    echo "<p>Kahit hindi mo alam</p>";
+    echo "<p>      F        Gm7      Bb</p>";
+    echo "<p>Ilang beses mo kong niligtas</p>";
+    echo "<p> Gm7            C              Dm</p>";
+    echo "<p>Ikaw ang hantungan, at aking wakas</p>";
+    echo "<br>";
+    echo "<p>[Instrumental]</p>";
+    echo "<p>C F</p>";
+    echo "<p>Dm C F</p>";
+    echo "<p>Dm C F</p>";
+    echo "<p>Dm C Bb C</p>";
+    echo "<br>";
+    echo "<p>[Bridge]</p>";
+    echo "<p>         Dm        C</p>";
+    echo "<p>Sana’y iyong matanggap</p>";
+    echo "<p>     F     Gm7  Bb   C</p>";
+    echo "<p>Kung sino ako talaga</p>";
+    echo "<br>";
+    echo "<p>[Chorus]</p>";
+    echo "<p>            F               C</p>";
+    echo "<p>Ikaw 'yong kumpas no'ng naliligaw</p>";
+    echo "<p>             Bb                 C</p>";
+    echo "<p>Naging kulay ka sa langit na bughaw</p>";
+    echo "<p>           F          C</p>";
+    echo "<p>Sa bawat bagyo na dumayo</p>";
+    echo "<p>              Bb                C</p>";
+    echo "<p>Ikaw 'yung kanlungan na nahanap ko</p>";
+    echo "<p>             Dm     C</p>";
+    echo "<p>Kahit no'ng 'di ko alam</p>";
+    echo "<p>      F        Gm7      Bb</p>";
+    echo "<p>Ilang beses mo kong niligtas</p>";
+    echo "<p> Gm7            C              F</p>";
+    echo "<p>Ikaw ang hantungan, at aking wakas</p>";
+
+    ?>
+    <a href="index.php">Go Back</a>
+</body>
+</html>
